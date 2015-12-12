7fd9e9a8585d75f5c4095b46c41921db99b6ab8d
kwask <kawasaki.masahiro@gmail.com>
2015-12-12 21:24:36 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+electron.crashReporter.start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  // Create the browser window
+  mainWindow = new BrowserWindow({width:800, height:600});
+
+  // load the index.html
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
